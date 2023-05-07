fe919ed884e1b2d03db2b5761f436e082b956df5
SanchezYoan <yoan30470@hotmail.fr>
2023-05-07 20:07:44 +0200

    countdown

---
+++ index.js
@@ -0,0 +1,38 @@
+
+let totalSeconds;
+let interval;
+
+function countDown() {
+    const minutes = Math.floor(totalSeconds / 60)
+    const seconds = totalSeconds % 60;
+    
+    const min = minutes < 10 ? "0" + minutes : minutes;
+    const sec = seconds < 10 ? "0" + seconds : seconds;
+
+    countdownDisplay.textContent = `${min} : ${sec}`
+    if (totalSeconds > 0) {
+        totalSeconds--;
+    } else {
+        countdownDisplay.textContent = "C'est terminé !"
+        clearInterval(interval)
+    }
+
+
+
+}
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    
+    if(isNaN(choice.value)) {
+        alert("Entrez un chiffre ou un nombre")
+    } else {
+        totalSeconds = choice.value * 60;
+        choice.value = ""
+        clearInterval(interval)
+        interval = setInterval(countDown, 100)
+    }
+
+    
+})
+
